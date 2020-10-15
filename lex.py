1922f1cbd4823c525f1eefd054db43c3c784777d
Caiocbeleza <caio.almeidabeleza@gmail.com>
2020-10-15 15:45:31 -0300

    adding tokens

--- lex.py
+++ lex.py
@@ -6,11 +6,34 @@ class Token(NamedTuple):
     kind: str
     value: str
 
-
 def lex(code: str) -> Iterable[Token]:
     """
     Retorna sequência de objetos do tipo token correspondendo à análise léxica
     da string de código fornecida.
     """
+
+    tokens = [
+        ("LPAR", r"\("),
+        ("RPAR", r"\)"),
+        ("CHAR", r"#\\[a-zA-Z]*"),
+        ("STRING", r"\".*\""),   
+        ("NUMBER", r"(\+|\-|)?\d+(\.\d*)?"),
+        ("NAME", r"([a-zA-Z_%\+\-]|\.\.\.)[a-zA-Z_0-9\-\>\?\!]*"),         
+        ("BOOL", r"#[t|f]"),
+        ("QUOTES", r"[\'\"]"),
+        ("COMMA", r","),
+        ("NEWLINE",  r"\\n"),
+
+    ]
+    
+    regex = '|'.join('(?P<%s>%s)' % pair for pair in tokens)
+
+    string = re.sub(r";;.*", "", code)
+
+        
+    for mo in re.finditer(regex, string):
+        kind = mo.lastgroup
+        value = mo.group()
+        yield Token(kind, value)
 
     return [Token('INVALIDA', 'valor inválido')]
